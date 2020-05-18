6f9e4f00aea65c7f122bba84894d08fa0fe9ce35
tyna1991 <justynasiurnicka@gmail.com>
2020-05-18 11:58:39 +0200

    select period change

--- src/components/detalis/Timetable.js
+++ src/components/detalis/Timetable.js
@@ -8,31 +8,55 @@ class Timetable extends React.Component{
     constructor(){
         super()
         this.state={
-            period:'',
+            index:0,
+            periods:['cały okres', '1 tydzień', '2 tygodnie', '30 dni'],
+            displayOptions:false,
         }
+        this.listOfPeriods = React.createRef();
+        this.displayOptions = this.displayOptions.bind(this)
+        this.selectPeriod = this.selectPeriod.bind(this)
+        this.handleClickOutside = this.handleClickOutside.bind(this)
     }
     check = <FontAwesomeIcon icon={faCheck} color="#d5d5d5"/>
+    displayOptions() {
+        this.setState((prevState)=>{
+            return{
+                displayOptions:!prevState.displayOptions
+            }
+        })
+    }
+    selectPeriod(index){
+        this.setState({
+            ...this.state,
+            index
+        }, ()=>{this.displayOptions()})
+    }
+    componentWillMount() { document.addEventListener("click", this.handleClickOutside, false)};
+    componentWillUnmount() { document.removeEventListener("click", this.handleClickOutside, false); }
+    handleClickOutside(e) {
+        if(!this.listOfPeriods.current.contains(e.target) && this.state.displayOptions) {
+            this.displayOptions();
+        }
+    }
     render(){
     return <div className="timetable">
-                    <h4>Timetable</h4>
-                <div className="period-change">
-                    <div className="select">
-                        <span>(this.state.period)</span>
+                <h4>Timetable</h4>
+                <div ref={this.listOfPeriods} className="period-change">
+                    <div onClick={this.displayOptions} className="select">
+                        <span>{this.state.periods[this.state.index]}</span>
                         <FontAwesomeIcon icon={faChevronDown} color="#d5d5d5"/>
                     </div>
-                    <div className="options">
-                        <ul>
-                            <li>cały okres{this.check}</li>
-                            <li>1 tydzień</li>
-                            <li>2 tygodnie</li>
-                            <li>30 dni</li>
-                        </ul>
+                    <div className="options" style={{display:this.state.displayOptions ? 'block' : 'none'}}>
+                       <ul>
+                       {this.state.periods.map((period, index)=>(
+                           <li key={index} onClick={()=>{this.selectPeriod(index)}}>{period}{this.state.index===index && this.check}</li>
+                       ))} 
+                       </ul>
                     </div>
                 </div>
                 <div className="bg" style={{height:"150px"}}>
 
                 </div>
-
             </div>
     }
 }
