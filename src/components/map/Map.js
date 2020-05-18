5c1a67c1f32f1b582665d0dfd79566afec99bb8e
tyna1991 <justynasiurnicka@gmail.com>
2020-05-18 12:12:50 +0200

    change handle of selected country

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -7,17 +7,24 @@ class Map extends React.Component{
     constructor(){
         super()
         this.state = {
-            country:''
+            selectedCountry:'',
         }
+        this.changeHandler = this.changeHandler.bind(this)
+    }
+    changeHandler(e){
+    const {value} = e.target;
+    this.setState({
+        selectedCountry:value
+    })
     }
     render(){
     return <div className="map">
             <div className="country-change">
                 <div className="select-wrapper">
-                    <select name="">
-                        <option>Państwo 1</option>
-                        <option>Państwo 2</option>
-                        <option>Państwo 3</option>
+                    <select onChange={this.changeHandler} name="selectedCountry">
+                        <option value='1'>Państwo 1</option>
+                        <option value='2'>Państwo 2</option>
+                        <option value='3'>Państwo 3</option>
                     </select>
                     <span className="search-button">
                     <FontAwesomeIcon icon={faSearch} color="#d40000"/>
