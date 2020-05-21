d989f3f30fbb7de13d89e77376b540cc0f44c58d
tyna1991 <justynasiurnicka@gmail.com>
2020-05-21 22:58:34 +0200

    implementation of alert

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import Detalis from './components/detalis/Detalis'
 import Map from './components/map/Map'
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
+function App(props) {
   return (
+    <>
+    {props.message && <p className="error-message">{props.message}</p>}
     <div className="App">
       <Map/>
       <Detalis/>
     </div>
+    </>
   );
 }
 
-export default App;
+function mapState(state) {
+  const { alert } = state;
+  const {message} = alert;
+  return { message };
+}
+
+const actionCreators = {}
+
+export default connect(mapState, actionCreators)(App);
--- src/reducers/alert.reducer.js
+++ src/reducers/alert.reducer.js
@@ -1,4 +1,4 @@
-export default function alert(state = {}, action) {
+export default function alert(state = {message:''}, action) {
     switch (action.type) {
       case 'ERROR':
         return {
