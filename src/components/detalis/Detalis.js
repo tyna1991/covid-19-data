88e67ad0868ae6a413f167b230ba6ad4fc4039e8
tyna1991 <justynasiurnicka@gmail.com>
2020-05-21 15:35:05 +0200

    description change

--- src/components/detalis/Detalis.js
+++ src/components/detalis/Detalis.js
@@ -6,9 +6,10 @@ import Timeline from './Timeline';
 function Detalis(){
     return <div className="detalis" id="scrollbar-custom">
         <h1>Tracking Coronavirus COVID-19</h1>
-        <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut dui non risus maximus posuere. 
-           Duis semper eros vel nisi suscipit mattis. Proin sit amet hendrerit magna. Etiam eu iaculis neque. 
-           Morbi dapibus porta dignissim. Integer ut fermentum leo, imperdiet varius felis. Etiam porttitor malesuada lectus vitae vestibulum. </p>
+        <p className="description">Coronavirus disease 2019 (COVID-19) is an <b>infectious disease</b> caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2). 
+        It was first identified in December 2019 in Wuhan, China, and has since spread globally, resulting in an <b>ongoing pandemic</b>.
+         As of 21 May 2020, <b>more than 5.01 million cases</b> have been reported across 188 countries and territories, resulting in more than 328,000 deaths. 
+         More than 1.9 million people have recovered.</p>
         <div className="country-detalis">
         <Statistics/>
         <Timeline/>
